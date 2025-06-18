b489dd4ab502a3879dc05348cb63f5692f4d52bb
Administrator <dankoshevoy2001@gmail.com>
2025-06-18 08:54:11 +0300

    Add initial Vue 3 setup with Vite, including Docker configuration and ESLint support

---
+++ services/frontend/src/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app_root" class="app-container">
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts"></script>
+
+<style lang="scss" scoped></style>
---
+++ services/frontend/src/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Hello World</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Home',
+})
+</script>
+
+<style scoped lang="scss"></style>
