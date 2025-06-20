f1f97785143387002c2db10778fc1bc548436997
Administrator <dankoshevoy2001@gmail.com>
2025-06-20 10:15:13 +0300

    Refactor Docker setup and implement initial Nginx configuration for auth service

--- services/auth-service/src/app/Http/Controllers/AuthController.php
+++ services/auth-service/src/app/Http/Controllers/AuthController.php
@@ -41,6 +41,7 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
+        return response()->json(['message' => 'Login functionality not implemented yet'], 501);
     }
 
     public function me(Request $request)
